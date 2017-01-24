cbd120747a83907020f6857c4708057647c6f36b
Anik1199 <anik9280@gmail.com>
2017-01-24 13:14:02 +0600

    markw: Create rfs symlink
    * Create symlink for IMS

--- Android.mk
+++ Android.mk
@@ -22,5 +22,17 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
-endif
+IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
 
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT)/app/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+$(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "IMS lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/vendor/lib64/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
+
+include device/xiaomi/markw/tftp.mk
+
+endif
