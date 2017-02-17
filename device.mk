ad4b824c20d6c83ac49a59db9a9ebf843e43f06f
TheScarastic <warabhishek@gmail.com>
2017-02-17 09:17:29 +0000

    markw: Modify keylayouts
        * Xiaomi seems to handle fingperint sensor touch event in userspace and hence provides no keylayouts in stock rom, so we write kls for it
        * Remove unneeded keylayouts as well
    
    Change-Id: I0a4db49e820abc3f2e43ad528b917e18cfcb763d

--- device.mk
+++ device.mk
@@ -169,9 +169,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/keylayout/synaptics_dsx.kl:system/usr/keylayout/synaptics_dsx.kl \
-    $(LOCAL_PATH)/keylayout/synaptics_dsxv26.kl:system/usr/keylayout/synaptics_dsxv26.kl \
-    $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/keylayout/gf3208.kl:system/usr/keylayout/gf3208.kl
 
 # Lights
 PRODUCT_PACKAGES += \
