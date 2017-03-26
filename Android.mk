9fd56f098de77bc1a04211d865921a775eee64e3
msdx321 <msdx321@gmail.com>
2017-03-26 20:05:26 +0800

    markw: Fix IMS Symlink path
        * Thanks Alexandr(Razziell@Github) for pointing this out

--- Android.mk
+++ Android.mk
@@ -56,7 +56,7 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 
 IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
 
-IMS_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_APPS)/app/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_APPS)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
 $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "IMS lib link: $@"
 	@mkdir -p $(dir $@)
